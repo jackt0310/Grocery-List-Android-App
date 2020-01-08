615d353240d05b2b238b8cdb14cca843f752fa78
Jack Thompson <54644847+jacktho0559@users.noreply.github.com>
2020-01-08 10:27:35 -0500

    Starting work on app

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private ArrayAdapter<String> itemList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView groceryList = (ListView) findViewById(R.id.listView);
+
+        itemList = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        groceryList.setAdapter(itemList);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String item = editText.getText().toString();
+        itemList.add(item);
+        itemList.notifyDataSetChanged();
+        editText.setText("");
+        /*
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+        */
+    }
+}
